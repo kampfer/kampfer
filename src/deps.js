a9e6009d301394fe52343d13b75610234e99b0fc
kampfer <l.w.kampfer@gmail.com>
2012-12-27 22:46:18 +0800

    update

--- src/deps.js
+++ src/deps.js
@@ -3,4 +3,5 @@ kampfer.addDependency('base.js', [], []);
 kampfer.addDependency('class.js', ['Class'], []);
 kampfer.addDependency('data.js', ['data'], ['browser.support']);
 kampfer.addDependency('events.js', ['events','events.Event','events.Listener'], ['data']);
+kampfer.addDependency('eventtarget.js', ['events.EventTarget'], ['events','Class']);
 kampfer.addDependency('support.js', ['browser.support'], []);
